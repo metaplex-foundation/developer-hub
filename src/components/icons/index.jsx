ddf324f47373aee27fe29cd3b5f50827360c92dc
Loris Leiva <loris.leiva@gmail.com>
2023-08-02 10:00:02 +0100

    Add JS and Rust icons

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -2,6 +2,14 @@ import * as outlineIcons24 from '@heroicons/react/24/outline'
 import * as solidIcons20 from '@heroicons/react/20/solid'
 import clsx from 'clsx'
 
+import { JavaScriptIcon } from './JavaScriptIcon'
+import { RustIcon } from './RustIcon'
+
+const customIcons = {
+  JavaScriptIcon: JavaScriptIcon,
+  RustIcon: RustIcon,
+}
+
 export function Icon({ icon, className, ...props }) {
   const isSolid = icon.startsWith('Solid')
   const icons = isSolid ? solidIcons20 : outlineIcons24
@@ -9,6 +17,8 @@ export function Icon({ icon, className, ...props }) {
   const IconComponent =
     icons[trimmedIcon] ??
     icons[trimmedIcon + 'Icon'] ??
+    customIcons[trimmedIcon + 'Icon'] ??
+    customIcons[trimmedIcon] ??
     icons['ExclamationCircleIcon']
   const defaultSize = isSolid ? 'h-5 w-5' : 'h-6 w-6'
 
--- src/pages/bubblegum/getting-started.md
+++ src/pages/bubblegum/getting-started.md
@@ -4,12 +4,12 @@ metaTitle: Bubblegum - Getting Started
 description: Get started with compressed NFTs.
 ---
 
-TODO
+Select the language / library you want to use below to get started with compressed NFTs.
 
 {% quick-links %}
 
-{% quick-link title="JavaScript (Umi)" icon="InboxArrowDown" href="/bubblegum/getting-started/js" description="TODO." /%}
+{% quick-link title="JavaScript" icon="JavaScript" href="/bubblegum/getting-started/js" description="Get started with our JavaScript library based on the Umi framework." /%}
 
-{% quick-link title="Rust" icon="CodeBracketSquare" href="/bubblegum/getting-started/rust" description="TODO." /%}
+{% quick-link title="Rust" icon="Rust" href="/bubblegum/getting-started/rust" description="Get started using our Rust crate." /%}
 
 {% /quick-links %}
