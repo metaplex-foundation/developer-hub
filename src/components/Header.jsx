1aae9ae31db9458553d3515eec5ff1f8e959b90d
Loris Leiva <loris.leiva@gmail.com>
2023-08-02 15:18:09 +0100

    Link GitHub icon to the product's repo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,7 +72,11 @@ export function Header({ page }) {
         </div>
         <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 lg:flex-grow">
           <ThemeSelector className="relative z-10" />
-          <Link href="https://github.com" className="group" aria-label="GitHub">
+          <Link
+            href={page.product.github}
+            className="group"
+            aria-label="GitHub"
+          >
             <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
           </Link>
         </div>
