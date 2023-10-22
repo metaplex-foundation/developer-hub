8e707eadc4700bb6bf980c7aa45dfc1dbf4e6ba6
Mark Sackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2023-10-22 21:01:26 +0200

    fix whitespaces

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -12,7 +12,8 @@ const VotedYes = () => {
           <div className="relative overflow-hidden rounded-xl p-6">
             <h2 className="font-display text-base text-slate-900 dark:text-white">
               <span className="absolute -inset-px rounded-xl"></span>Thanks for
-              your feedback. We are glad you like it :)
+              your feedback. <br />
+              We are glad you like it :)
             </h2>
           </div>
         </div>
@@ -29,9 +30,10 @@ const VotedNo = () => {
           <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 opacity-100 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_bottom,var(--quick-links-hover-border))_border-box] dark:[--quick-links-hover-bg:theme(colors.slate.800)]"></div>
           <div className="relative overflow-hidden rounded-xl p-6">
             <h2 className="font-display text-base text-slate-900 dark:text-white">
-              <span className="absolute -inset-px rounded-xl"></span>Thanks for
-              your feedback. We will try to improve :(
+              <span className="-inset-px rounded-xl"></span>Thanks for
+              your feedback. We will try to improve :( <br />
             </h2>
+            <a href='https://metaplex.canny.io/developers?selectedCategory=documentation#introduction'>Do you want to give additional feedback?</a>
           </div>
         </div>
       </div>
@@ -86,7 +88,7 @@ export default function Feedback({ resource }) {
     <>
       <div
         id="happyReact"
-        className="prose prose-slate max-w-none dark:prose-invert prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal prose-a:font-semibold prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.accent.300))] hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg prose-table:m-0 prose-lead:text-slate-500 dark:text-slate-400 dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:text-accent-400 dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.accent.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800 dark:prose-lead:text-slate-400 lg:prose-headings:scroll-mt-[8.5rem]"
+        className="prose prose-slate max-w-none dark:prose-invert prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal prose-a:font-semibold prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.accent.300))] hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg prose-table:m-0 prose-lead:text-slate-500 dark:text-slate-400 dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:text-accent-400 dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.accent.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800 dark:prose-lead:text-slate-400 lg:prose-headings:scroll-mt-[8.5rem] mt-5"
       >
         <h2 className="mb-0 text-center">Was this page helpful?</h2>
         {!isReacted ? (
@@ -96,10 +98,11 @@ export default function Feedback({ resource }) {
               resource={_resource}
               onReaction={handleReaction}
               classes={{
+                container: 'mt-5 md:mt-0',
                 grid: 'not-prose grid grid-cols-1 gap-6 sm:grid-cols-2',
                 cell: 'group relative rounded-xl border border-slate-200 dark:border-slate-800',
                 reaction:
-                  'relative overflow-hidden rounded-xl p-6 font-semibold',
+                  'relative overflow-hidden rounded-xl p-6 font-semibold w-full',
               }}
             />
           </div>
