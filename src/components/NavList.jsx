292b0ae90149c51812e70ed2064a0e23d4f1dfcf
Tony Boyle <81017245+tonyboylehub@users.noreply.github.com>
2024-02-11 13:05:37 -0700

    nav position fix

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,4 +1,5 @@
 import { Popover } from '@headlessui/react'
+import { useState } from 'react'
 import { Logo, LogoWithName } from './products/Logo'
 import { SwitcherDialog } from './products/SwitcherDialog'
 import { SwitcherPopover } from './products/SwitcherPopover'
@@ -6,6 +7,10 @@ import { SwitcherPopover } from './products/SwitcherPopover'
 const menuItems = ['ACM', 'Commerce', 'Utility', 'Dev Tools']
 
 const NavList = () => {
+
+  const [active, setActive] = useState();
+
+
   return (
     <div className="hidden cursor-pointer  gap-8 lg:flex">
       {menuItems.map((item, index) => {
@@ -13,7 +18,7 @@ const NavList = () => {
           <>
             <div className="hidden flex-col lg:flex">
               <SwitcherPopover menuItem={menuItems[index]}>
-                <Popover.Button className="-mx-4 -my-2 rounded-lg px-4 py-2 text-white">
+                <Popover.Button className="-mx-4 -my-2 rounded-lg px-4 py-2 text-black dark:text-white" onClick={() => setActive(index)}>
                   {item}
                   {/* <Logo product={page.product} className="h-8 w-8 sm:hidden" />
                 <LogoWithName
--- src/components/products/SwitcherPopover.jsx
+++ src/components/products/SwitcherPopover.jsx
@@ -4,28 +4,32 @@ import { Popover, Transition } from '@headlessui/react'
 export function SwitcherPopover({ children, menuItem, ...props }) {
   console.log('menuItem', menuItem)
   return (
-    <Popover className="relative" {...props}>
+    <Popover {...props}>
       {children}
-      <Transition
+      {/* <Transition
         enter="transition ease-out duration-200"
         enterFrom="opacity-0 translate-y-1"
         enterTo="opacity-100 translate-y-0"
         leave="transition ease-in duration-150"
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
-      >
-        <Popover.Panel className="absolute z-10 mt-4 w-max">
-          {({ close }) => (
-            <div className="absolute -left-[225px] w-[530px] overflow-hidden rounded-lg bg-white p-4 shadow-xl ring-1 ring-black ring-opacity-5 dark:border dark:border-slate-600 dark:bg-slate-800">
+      > */}
+      <Popover.Panel className="absolute z-10 m-auto mt-4 ">
+        {({ close }) => (
+          <div className="fixed left-0 w-full">
+            <div
+              className="m-auto w-full max-w-[600px]  overflow-hidden  rounded-lg bg-white p-4 shadow-xl ring-1 ring-black ring-opacity-5 dark:border dark:border-slate-600 dark:bg-slate-800"
+            >
               <Grid
                 className="relative md:grid-flow-row md:grid-cols-2"
                 onClick={close}
                 menuItem={menuItem}
               />
             </div>
-          )}
-        </Popover.Panel>
-      </Transition>
+          </div>
+        )}
+      </Popover.Panel>
+      {/* </Transition> */}
     </Popover>
   )
 }
