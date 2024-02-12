5a6f057186089716083ee5ee8458d10b71fedb60
Tony Boyle <81017245+tonyboylehub@users.noreply.github.com>
2024-02-11 17:21:02 -0700

    initial commit

---
+++ src/components/products/das-api/index.js
@@ -0,0 +1,38 @@
+import {
+  changelogSection,
+  documentationSection,
+  recipesSection,
+  referencesSection,
+} from '@/shared/sections'
+import { Hero } from './Hero'
+import { Logo } from './Logo'
+
+export const das = {
+  name: 'DAS Api',
+  headline: 'DAS API',
+  description:
+    'A DAS API Client for access the Digital Asset Standard',
+  path: 'das-api',
+  logo: Logo,
+  github: 'https://github.com/metaplex-foundation/digital-asset-standard-api',
+  className: 'accent-sky',
+  heroes: [{ path: '/das-api', component: Hero }],
+  sections: [
+    {
+      ...documentationSection('das-api'),
+      navigation: [
+        {
+          title: 'Introduction',
+          links: [
+            { title: 'Overview', href: '/das-api' },
+            { title: 'Getting started', href: '/das-api/getting-started' },
+           
+            { title: 'Methods', href: '/das-api/methods' },
+
+          ],
+        },
+      ],
+    },
+    { ...changelogSection('das-api') },
+  ],
+}
