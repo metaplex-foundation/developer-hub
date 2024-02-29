e2656da81d3e5f064c8bbcd632a4a3abecba4eb8
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2024-02-29 11:13:55 +0100

    add Curl to DAS API (#48)
    * cleanup case
    
    * split pages add curl
    
    * Update index.js
    
    Fixed duplicate url/nav link
    
    ---------
    
    Co-authored-by: Tony Boyle <81017245+tonyboylehub@users.noreply.github.com>

--- src/components/products/das-api/index.js
+++ src/components/products/das-api/index.js
@@ -8,10 +8,10 @@ import { Hero } from './Hero'
 import { Logo } from './Logo'
 
 export const das = {
-  name: 'DAS Api',
-  headline: 'DAS API',
+  name: 'DAS API',
+  headline: 'Fetch Digital Asset Data',
   description:
-    'A DAS API Client for access the Digital Asset Standard',
+    'A DAS API Client to access Digital Asset data on chain',
   path: 'das-api',
   logo: Logo,
   github: 'https://github.com/metaplex-foundation/digital-asset-standard-api',
@@ -26,10 +26,20 @@ export const das = {
           title: 'Introduction',
           links: [
             { title: 'Overview', href: '/das-api' },
-            { title: 'Getting started', href: '/das-api/getting-started' },
-           
-            { title: 'Methods', href: '/das-api/methods' },
-
+            { title: 'Getting started', href: '/das-api/getting-started' },     
+          ],
+        },
+        {
+          title: 'Methods',
+          links: [
+            { title: 'Method Overview', href: '/das-api/methods' },
+            { title: 'Get Asset', href: '/das-api/methods/get-asset' },
+            { title: 'Get Asset Proof', href: '/das-api/methods/get-asset-proof' },
+            { title: 'Get Asset By Authority', href: '/das-api/methods/get-asset-by-authority' },
+            { title: 'Get Asset By Creator', href: '/das-api/methods/get-asset-by-creator' },
+            { title: 'Get Asset By Group', href: '/das-api/methods/get-asset-by-group' },
+            { title: 'Get Asset By Owner', href: '/das-api/methods/get-asset-by-owner' },
+            { title: 'Search Asset', href: '/das-api/methods/search-assets' },
           ],
         },
       ],
--- src/pages/das-api/index.md
+++ src/pages/das-api/index.md
@@ -1,7 +1,7 @@
 ---
-title: DAS Api
-metaTitle: DAS Api - Overview
-description: A DAS Api client used to access the Metaplex Digital Asset Standard.
+title: DAS API
+metaTitle: DAS API - Overview
+description: A DAS API client used to access the Metaplex Digital Asset Standard.
 ---
 
 The state data of uncompressed NFTs is all stored in on-chain accounts. This is expensive at scale. Compressed NFTs save space by encoding the state data into an on-chain Merkle tree. The detailed account data is not stored on-chain, but in data stores managed by RPC providers. The Metaplex Digital Asset Standard (DAS) API represents a unified interface for interacting with digital assets on Solana, supporting both standard (Token Metadata) and compressed (Bubblegum) assets.
@@ -12,6 +12,6 @@ The API defines a set of methods that RPCs implement in order to provide asset d
 
 {% quick-link title="Getting Started" icon="InboxArrowDown" href="/das-api/getting-started" description="Find the language or library of your choice and get started essentials programs." /%}
 
-{% quick-link title="Methods" icon="CodeBracketSquare" href="/das-api/methods" description="DAS Api methods for fetching data." /%}
+{% quick-link title="Methods" icon="CodeBracketSquare" href="/das-api/methods" description="DAS API methods for fetching data." /%}
 
 {% /quick-links %}
