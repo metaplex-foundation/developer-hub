1c2cd0c6347fa0c54521b5451a2b57b238a0e4d4
blockiosaurus <blockiosaurus@gmail.com>
2024-03-13 10:51:20 -0400

    Adding guides section.

--- src/shared/sections.js
+++ src/shared/sections.js
@@ -13,6 +13,13 @@ export const referencesSection = (productSlug) => ({
   href: `/${productSlug}/references`,
 })
 
+export const guidesSection = (productSlug) => ({
+  id: 'guides',
+  title: 'Guides',
+  icon: 'SolidRectangleStack',
+  href: `/${productSlug}/guides`,
+})
+
 export const recipesSection = (productSlug) => ({
   id: 'recipes',
   title: 'Recipes',
