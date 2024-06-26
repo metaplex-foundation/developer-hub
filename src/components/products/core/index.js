33c251c39bf22ebabfdcd6c7b3c4c18b8b18c6ad
L0STE <125566964+L0STE@users.noreply.github.com>
2024-06-26 10:32:43 +0200

    Updated title

--- src/components/products/core/index.js
+++ src/components/products/core/index.js
@@ -170,7 +170,7 @@ export const core = {
             { title: 'Immutability', href: '/core/guides/immutability' },
             { title: 'Print Editions', href: '/core/guides/print-editions' },
             { title: 'Oracle Plugin Example', href: '/core/guides/oracle-plugin-example' },
-            { title: 'Typescript Staking Example', href: '/core/guides/typescript-staking-example' }
+            { title: 'Web2 typescript Staking Example', href: '/core/guides/javascript/web2-typescript-staking-example' }
           ],
         },
       ],
