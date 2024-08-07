2094ca636707060124f0ae814abfdca9b1f89b00
tonyboylehub <tony@tonyboyle.co.uk>
2024-08-07 17:36:01 +0100

    lfg! Working, removed console.logs(), added main comment, removed subcomments

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,75 +1,13 @@
 import fs from 'fs'
 import path from 'path'
 
-// export async function generateSitemap() {
-//   let directoryPath = path.join(process.cwd(), 'src/pages').replace(/\\/g, '/')
-
-//   let files = []
-
-//   function throughDirectory(directory) {
-//     fs.readdirSync(directory).forEach((file) => {
-//       const Absolute = path.join(directory, file)
-//       if (fs.statSync(Absolute).isDirectory()) return throughDirectory(Absolute)
-//       else return files.push(Absolute)
-//     })
-//   }
-
-//   throughDirectory(path.join(process.cwd(), 'src/pages'))
-
-//   console.log(directoryPath)
-
-//   const filter = [
-//     '/api',
-//     '_app.jsx',
-//     '_document.jsx',
-//     '_error.jsx',
-//     'sitemap.xml.js',
-//   ]
-
-//   // List of all pages to be included in the sitemap
-//   const staticPages = files
-//     .filter((staticPage) => {
-//       return !filter.some((f) => staticPage.includes(f))
-//     })
-//     .map((staticPagePath) => {
-//       const path = staticPagePath
-//         .replace(/\\/g, '/')
-//         .replace(directoryPath, '')
-//         .replace('.jsx', '')
-//         .replace('.js', '')
-//         .replace('.md', '')
-//         .replace('//', '/')
-//       return `${`https://developers.metaplex.com`}${path}`
-//     })
-
-//   // Include dynamic pages if you have any
-//   // Example: Fetch dynamic routes from your CMS or database
-
-//   // Combine static and dynamic pages
-//   const allPages = [...staticPages]
-
-//   // Generate the sitemap XML
-//   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-//       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-//         ${allPages
-//           .map((url) => {
-//             return `
-//               <url>
-//                 <loc>${url}</loc>
-//               </url>
-//             `
-//           })
-//           .join('')}
-//       </urlset>`
-
-// //   fs.writeFileSync('public/sitemap.xml', sitemap)
-// //   console.log('Sitemap generated!')
-// }
+// Turns out Vercel builds the site into a different directory and file structure than running `next build` locally
+// So we need to read the routes-manifest.json file from a different location than the .next directory that is locally
+// built. The /sitemap.xml route will not work on local development, but it will work on Vercel deployment.
 
 export async function getServerSideProps({ res }) {
   console.log('running sitemap generation')
 
-  // let directoryPath = path.join(process.cwd(), 'src/pages').replace(/\\/g, '/')
 
   let directoryPath = path
     .join(process.cwd(), '.next/routes-manifest.json')
@@ -77,59 +15,9 @@ export async function getServerSideProps({ res }) {
 
   const json = JSON.parse(fs.readFileSync(directoryPath, 'utf8'))
 
-  console.log({ json })
-
   const staticRoutes = json.dataRoutes.map(route => {
     return route.page
   })
-
-  console.log({ staticRoutes })
-
-  // console.log({ directoryPath })
-
-  // let files = []
-
-  // function throughDirectory(directory) {
-  //   const fileDirectory = fs.readdirSync(directory)
-
-  //   console.log({ fileDirectory })
-  //   fileDirectory.forEach((file) => {
-  //     const Absolute = path.join(directory, file)
-  //     console.log({ Absolute })
-  //     if (fs.statSync(Absolute).isDirectory()) return throughDirectory(Absolute)
-  //     else return files.push(Absolute)
-  //   })
-  // }
-
-  // throughDirectory(path.join(directoryPath))
-
-  // console.log(files)
-
-  // const filter = ['.html']
-
-  // // List of all pages to be included in the sitemap
-  // const staticPages = files
-  //   .filter((staticPage) => {
-  //     return filter.some((f) => staticPage.includes(f))
-  //   })
-  //   .map((staticPagePath) => {
-  //     const path = staticPagePath
-  //       .replace(/\\/g, '/')
-  //       .replace(directoryPath, '')
-  //       .replace('.html', '')
-  //       //   .replace('.js', '')
-  //       //   .replace('.md', '')
-  //       .replace('//', '/')
-  //     return `${`https://developers.metaplex.com`}${path}`
-  //   })
-
-  // Include dynamic pages if you have any
-  // Example: Fetch dynamic routes from your CMS or database
-
-  // Combine static and dynamic pages
-  // const allPages = [...staticPages]
-
-  // console.log({ allPages })
 
   // Generate the sitemap XML
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
