b1ec01b0c8c704e543e9c1176564d9ba77a64843
L0STE <125566964+L0STE@users.noreply.github.com>
2024-08-14 12:40:15 +0200

    Added the Middleware file!

---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse,  } from 'next/server'
+
+const redirectData = {
+  "/umi/web3js-adapters": {
+    "destination": "/umi/web3js-differences-and-adapters",
+    "permanent": true
+  },
+  "/umi/web3js-differences": {
+    "destination": "/umi/web3js-differences-and-adapters",
+    "permanent": true
+  },
+}
+
+export async function middleware(request) {
+  const pathname = request.nextUrl.pathname
+  const redirectEntry = redirectData[pathname]
+
+ 
+  if (redirectEntry ) {
+    const statusCode = redirectEntry.permanent ? 308 : 307
+    return NextResponse.redirect(request.nextUrl.origin + redirectEntry.destination, statusCode)
+  }
+ 
+  return NextResponse.next()
+}
