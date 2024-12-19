8ce061b2deefcae1f9fd38e00c4933c1eafb5b84
tonyboylehub <tony@tonyboyle.co.uk>
2024-12-19 16:26:19 +0000

    renderer fix

--- src/components/apiComponents/languageRenderer.jsx
+++ src/components/apiComponents/languageRenderer.jsx
@@ -1,11 +1,10 @@
-import { useCallback, useState } from 'react'
-import EndPointSelector, { endpoints } from './endPointSelector'
+import { useState } from 'react'
+import EndPointSelector from './endPointSelector'
 import CSharpRequestRenderer from './languageComponents/cSharpRenderer'
 import CurlRequestRenderer from './languageComponents/curlRequestRenderer'
 import GoRequestRenderer from './languageComponents/goRequestRenderer'
 import JavaRenderer from './languageComponents/javaRenderer'
 import JavascriptRequestRenderer from './languageComponents/javascriptRequestRenderer'
-import KotlinRenderer from './languageComponents/kotlinRenderer'
 import PhpRenderer from './languageComponents/phpRenderer'
 import PythonRequestRenderer from './languageComponents/pythonRequestRenderer'
 import RubyRenderer from './languageComponents/rubyRenderer'
@@ -13,19 +12,19 @@ import RustRequestRenderer from './languageComponents/rustRenderer'
 import SwiftRequestRenderer from './languageComponents/swiftRenderer'
 import LanguageSelector from './languageSelector'
 
-const LanguageRenderer = ({ api, body, setActiveEndPoint, activeEndPoint }) => {
+const LanguageRenderer = ({ api, body, setActiveEndpoint, activeEndpoint }) => {
   const [activeLanguage, setActiveLanguage] = useState('javascript')
 
   function strToTitleCase(str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
   }
 
-  console.log({activeEndPoint})
-
-  const renderLanguage = (activeLanguage, activeEndpoint) => {
+  const renderLanguage = (activeLanguage) => {
     const headers = api.headers
       ? api.headers
       : { 'Content-Type': 'application/json' }
+
+    console.log({ activeEndpoint })
 
     switch (activeLanguage) {
       case 'javascript':
@@ -36,7 +35,7 @@ const LanguageRenderer = ({ api, body, setActiveEndPoint, activeEndPoint }) => {
             </div>
             <JavascriptRequestRenderer
               method={api.method}
-              url={activeEndPoint.uri}
+              url={activeEndpoint.uri}
               headers={headers}
               bodyMethod={body.method}
               bodyParams={body.params}
@@ -68,7 +67,7 @@ const LanguageRenderer = ({ api, body, setActiveEndPoint, activeEndPoint }) => {
             </div>
             <CurlRequestRenderer
               method={api.method}
-              url={end}
+              url={activeEndpoint.uri}
               headers={headers}
               bodyMethod={body.method}
               bodyParams={body.params}
@@ -99,8 +98,8 @@ const LanguageRenderer = ({ api, body, setActiveEndPoint, activeEndPoint }) => {
               {strToTitleCase(activeLanguage)} Request Example
             </div>
             <CSharpRequestRenderer
-              method={endpoint}
-              url={activeEndPoint}
+              method={api.method}
+              url={activeEndpoint.uri}
               // headers={headers}
               bodyMethod={body.method}
               bodyParams={body.params}
@@ -115,8 +114,8 @@ const LanguageRenderer = ({ api, body, setActiveEndPoint, activeEndPoint }) => {
               {strToTitleCase(activeLanguage)} Request Example
             </div>
             <JavaRenderer
-              method={endpoint}
-              url={activeEndPoint}
+              method={api.method}
+              url={activeEndpoint.uri}
               headers={headers}
               bodyMethod={body.method}
               bodyParams={body.params}
@@ -205,8 +204,8 @@ const LanguageRenderer = ({ api, body, setActiveEndPoint, activeEndPoint }) => {
   return (
     <div className="flex w-full flex-col gap-8 overflow-hidden">
       <EndPointSelector
-        setActiveEndpoint={(endpoint) => setActiveEndPoint(endpoint)}
-        activeEndpoint={activeEndPoint}
+        setActiveEndpoint={(endpoint) => setActiveEndpoint(endpoint)}
+        activeEndpoint={activeEndpoint}
       />
       <LanguageSelector
         activeLanguage={activeLanguage}
