efaafa54c328969cd9ad0311786b6f52970706bd
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2025-04-14 11:53:21 +0200

    add displayOptions (#346)

--- src/lib/api/aura/das/getAssetsByGroup.js
+++ src/lib/api/aura/das/getAssetsByGroup.js
@@ -1,59 +1,89 @@
-
 const getAssetsByGroup = {
-    description: 'Returns the list of assets given an Group address.',
-    method: 'getAssetsByGroup',
-    params: [
-      {
-        name: 'groupKey',
-        type: 'string',
-        description: 'Grouping Key',
-        placeholder: 'collection',
-        required: true,
-      },
-      {
-        name: 'groupValue',
-        type: 'string',
-        description: 'Value of the group. E.g. Public key of the collection',
-        required: true,
-      },
-      {
-        name: 'sortBy',
-        type: 'object',
-        description: 'Sorting criteria',
-        value: {
-          sortBy: {
-            type: 'option',
-            value: ['created', 'updated', 'recentAction', 'none'],
-          },
-          sortDirection: {
-            type: 'option',
-            value: ['asc', 'desc'],
-          },
+  description: 'Returns the list of assets given an Group address.',
+  method: 'getAssetsByGroup',
+  params: [
+    {
+      name: 'groupKey',
+      type: 'string',
+      description: 'Grouping Key',
+      placeholder: 'collection',
+      required: true,
+    },
+    {
+      name: 'groupValue',
+      type: 'string',
+      description: 'Value of the group. E.g. Public key of the collection',
+      required: true,
+    },
+    {
+      name: 'sortBy',
+      type: 'object',
+      description: 'Sorting criteria',
+      value: {
+        sortBy: {
+          type: 'option',
+          value: ['created', 'updated', 'recentAction', 'none'],
+        },
+        sortDirection: {
+          type: 'option',
+          value: ['asc', 'desc'],
         },
       },
-      {
-        name: 'limit',
-        type: 'number',
-        description: 'Number of assets to return',
+    },
+    {
+      name: 'limit',
+      type: 'number',
+      description: 'Number of assets to return',
+    },
+    {
+      name: 'page',
+      type: 'number',
+      description: 'The index of the "page" to retrieve.',
+    },
+    {
+      name: 'before',
+      type: 'string',
+      description: 'Retrieve assets before the specified ID',
+    },
+    {
+      name: 'after',
+      type: 'string',
+      description: 'Retrieve assets after the specified ID',
+    },
+    {
+      name: 'options',
+      type: 'object',
+      description: 'Display options',
+      value: {
+        showCollectionMetadata: {
+          type: 'boolean',
+          description: 'Show collection metadata',
+          value: ['false', 'true'],
+        },
+        showFungible: {
+          type: 'boolean',
+          description: 'Show fungible assets',
+          value: ['false', 'true'],
+        },
+        showInscription: {
+          type: 'boolean',
+          description: 'Show inscription data',
+          value: ['false', 'true'],
+        },
+        showUnverifiedCollections: {
+          type: 'boolean',
+          description: 'Show unverified collections',
+          value: ['false', 'true'],
+        },
+        showZeroBalance: {
+          type: 'boolean',
+          description: 'Show token accounts with zero balance',
+          value: ['false', 'true'],
+        },
       },
-      {
-        name: 'page',
-        type: 'number',
-        description: 'The index of the "page" to retrieve.',
-      },
-      {
-        name: 'before',
-        type: 'string',
-        description: 'Retrieve assets before the specified ID',
-      },
-      {
-        name: 'after',
-        type: 'string',
-        description: 'Retrieve assets after the specified ID',
-      },
-    ],
-    examples: null,
-  }
-  
-  export default getAssetsByGroup
-  
+    },
+  ],
+  examples: null,
+}
+
+export default getAssetsByGroup
